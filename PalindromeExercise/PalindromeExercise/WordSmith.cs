a10303f7bc013aaa582f130c470e962b84cb483f
Austin Okyere <okyereaustin@gmail.com>
2026-02-12 20:17:08 -0700

    Completed palindrome exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using System;
+
+namespace PalindromeExercise;
+
+public class WordSmith
+{
+    public bool IsPalindrome(string word)
+    {
+        var lower =  word.ToLower();
+        char[] array = word.ToCharArray();
+        Array.Reverse(array);
+        var reversed = new string(array);
+        return lower == reversed;
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("mom", true)]
+        [InlineData("tuesday", false)]
+        [InlineData("saturday", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
-
+            WordSmith smith = new WordSmith();
+            
+            var actual = smith.IsPalindrome(word);
+            
+            Assert.Equal(expected, actual);
         }
+        
+        
+        
     }
 }
